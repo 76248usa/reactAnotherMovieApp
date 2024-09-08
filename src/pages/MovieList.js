9b4c460d05c19b2d6960912e4b7005e321e16120
76248usa <elsabecrous@Elsabes-MacBook-Pro.local>
2024-09-08 11:45:25 -0500

    first commit

---
+++ src/pages/MovieList.js
@@ -0,0 +1,22 @@
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components";
+import { useEffect } from "react";
+
+export const MovieList = ({ apiPath }) => {
+  useEffect(() => {
+    document.title = `Movies / Cinemate`;
+  });
+  const { data: movies } = useFetch(apiPath);
+
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
---
+++ src/pages/Search.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components";
+import { useSearchParams } from "react-router-dom";
+
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(apiPath, queryTerm);
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
